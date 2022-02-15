41b95371515a74c2cac04ea2862474ba6f2140c1
Neil <neilrowland56@yahoo.com>
2022-02-15 16:03:37 +0000

    Updated Header for black background, white text and active class-red

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import BlurCircularIcon from '@mui/icons-material/BlurCircular';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { useRouter } from 'next/router';
 
 function Header() {
+	const router = useRouter();
+
 	let Links = [
 		{ name: 'Home', link: '/' },
 		{ name: 'Services', link: '/services' },
@@ -16,8 +19,8 @@ function Header() {
 
 	return (
 		<div className="shadow-md  w-full fixed top-0 left-0">
-			<div className="md:flex items-center justify-between bg-blue-100 py-4 md:px-10 px-7">
-				<div className="font-bold text-2x1 cursor-pointer flex items-center font-poppins  text-gray-800">
+			<div className="md:flex items-center justify-between bg-[#040714] py-4 md:px-10 px-7">
+				<div className="font-bold text-2x1 cursor-pointer text-white hover:text-red-600 flex items-center font-poppins  ">
 					<span className="text-3x1 text-indigo-600 mr-1 ">
 						<BlurCircularIcon />
 					</span>
@@ -27,25 +30,31 @@ function Header() {
 					onClick={() => setOpen(!open)}
 					className="text-3x1 absolute right-8 top-6 cursor-pointer md:hidden"
 				>
-					<span className={open ? 'hidden' : ''}>{<MenuIcon />}</span>
+					<span className={open ? 'hidden' : ''}>
+						{<MenuIcon className="text-white" />}
+					</span>
 					<span className={open ? '' : 'hidden'}>
-						{<CloseIcon />}
+						{<CloseIcon className="text-white" />}
 					</span>
 				</div>
 
 				<ul
-					className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+					className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#040714] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
 						open ? 'top-20 ' : 'top-[-490px]'
 					}`}
 				>
 					{Links.map((link) => (
 						<li
 							key={link.name}
+							href={link.link}
 							className="md:ml-8 text-xl md:my-0 my-7"
 						>
 							<a
-								href={link.link}
-								className="text-gray-800 hover:text-red-600 duration-300"
+								className={`cursor-pointer ${
+									router.pathname === link.link
+										? 'text-red-600 hover:text-blue-500'
+										: 'text-white hover:text-blue-500'
+								}`}
 							>
 								{link.name}
 							</a>
